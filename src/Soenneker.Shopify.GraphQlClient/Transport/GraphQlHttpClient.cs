a065e5a109bf254b538956123ecd02cb59b58b40
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-09-07 16:58:42 +0000

    Automated update

--- src/Soenneker.Shopify.GraphQlClient/Transport/GraphQlHttpClient.cs
+++ src/Soenneker.Shopify.GraphQlClient/Transport/GraphQlHttpClient.cs
@@ -17,7 +17,7 @@ public sealed class GraphQlHttpClient : IGraphQlClient
     public GraphQlHttpClient(HttpClient httpClient, JsonSerializerOptions? serializerOptions = null)
     {
         _httpClient = httpClient;
-        _serializerOptions = serializerOptions ?? JsonSerializerOptions.Web;
+        _serializerOptions = serializerOptions ?? new JsonSerializerOptions(JsonSerializerDefaults.Web);
     }
 
     public async ValueTask<GraphQlResponse<T>> Execute<T>(
